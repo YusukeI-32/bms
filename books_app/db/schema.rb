8e47a513c930e10119a9cd015eef1e1ae1634408
Yusuke <now.good.1206@gmail.com>
2020-11-26 21:14:29 +0900

    ログイン機能実装したい

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_075230) do
+ActiveRecord::Schema.define(version: 2020_11_26_112854) do
 
 # Could not dump table "books" because of following StandardError
 #   Unknown type 'String' for column 'picture'
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
