606db3b92a852c333b31c3f25c7dd6bd106627c6
Yusuke <now.good.1206@gmail.com>
2020-11-26 17:58:09 +0900

    投稿時間追加

--- books_app/config/application.rb
+++ books_app/config/application.rb
@@ -16,7 +16,7 @@ module BooksApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.time_zone = 'Asis/Tokyo'
+    config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
   end
 end
